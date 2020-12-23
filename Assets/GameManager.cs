3e9ca2cf9525cc15e4de1f12552c97217523fea0
Filipa Correia <filipaincorreia@gmail.com>
2020-12-23 09:14:14 +0000

    Fix pile update after mistake.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,15 +7,22 @@ public class GameManager : MonoBehaviour
 {
     public int NumPlayers;
     public int Level;
+    public GameObject LevelUI;
     public int Lives;
-    public GameObject LevelTextUI;
-    public GameObject Overlay;
+    public GameObject LivesUI;
+    public GameObject OverlayNextLevel;
     public GameObject P0cards;
     public GameObject P1cards;
     public GameObject P2cards;
     public GameObject Pile;
     private List<int> pile;
     private List<List<int>> players;
+    public GameObject OverlayMistake;
+    public GameObject ContinueButton;
+    public GameObject GameOverText;
+    public GameObject P0wrongCards;
+    public GameObject P1wrongCards;
+    public GameObject P2wrongCards;
 
     // Start is called before the first frame update
     void Start()
@@ -33,31 +40,90 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Overlay.activeSelf)
+        if (!OverlayNextLevel.activeSelf && !OverlayMistake.activeSelf)
         {
             if (Input.GetKeyDown("a") && players[0].Count > 0)
             {
                 int nextCard = players[0][0];
                 pile.Add(nextCard);
                 players[0].RemoveAt(0);
+                UpdateCardsUI();
+                UpdatePileUI();
+                ValidateMove();
             }
             else if (Input.GetKeyDown("l") && players[1].Count > 0)
             {
                 int nextCard = players[1][0];
                 pile.Add(nextCard);
                 players[1].RemoveAt(0);
+                UpdateCardsUI();
+                UpdatePileUI();
+                ValidateMove();
             }
             else if (Input.GetKeyDown("space") && players[2].Count > 0)
             {
                 int nextCard = players[2][0];
                 pile.Add(nextCard);
                 players[2].RemoveAt(0);
+                UpdateCardsUI();
+                UpdatePileUI();
+                ValidateMove();
             }
-
+        }
+        if (!OverlayNextLevel.activeSelf && !OverlayMistake.activeSelf)
+        {
             UpdateCardsUI();
             UpdatePileUI();
+            CheckEndOfLevel();
         }
-        CheckEndOfLevel();
+    }
+
+    void ValidateMove()
+    {
+        int lastCard = pile[pile.Count - 1];
+        bool mistake = false;
+        List<string> wrongCardsUI = new List<string>();
+
+        for (int i = 0; i < NumPlayers; i++)
+        {
+            string wrongCards = "[";
+            while (players[i].Count > 0 && players[i][0] < lastCard)
+            {
+                mistake = true;
+                if (wrongCards != "[")
+                {
+                    wrongCards += ",";
+                }
+                wrongCards += players[i][0];
+                players[i].RemoveAt(0);
+            }
+            wrongCards += "]";
+            wrongCardsUI.Add(wrongCards);
+        }
+        if (mistake)
+        {
+            P0wrongCards.GetComponent<Text>().text = wrongCardsUI[0];
+            P1wrongCards.GetComponent<Text>().text = wrongCardsUI[1];
+            P2wrongCards.GetComponent<Text>().text = wrongCardsUI[2];
+            Lives--;
+            LivesUI.GetComponent<Text>().color = new Color(1, 0, 0);
+            Pile.GetComponent<Text>().color = new Color(1, 0, 0);
+            UpdateLivesUI();
+            OverlayMistake.SetActive(true);
+            if (Lives == 0)
+            {
+                ContinueButton.SetActive(false);
+                GameOverText.SetActive(true);
+            }
+        }
+    }
+
+    public void ContinueAfterMistake()
+    {
+        LivesUI.GetComponent<Text>().color = new Color(0, 0, 0);
+        Pile.GetComponent<Text>().color = new Color(0, 0, 0);
+        UpdateLivesUI();
+        OverlayMistake.SetActive(false);
     }
 
     public void NextLevel()
@@ -66,14 +132,14 @@ public class GameManager : MonoBehaviour
         DealCards();
         UpdateCardsUI();
         UpdatePileUI();
-        Overlay.SetActive(false);
+        OverlayNextLevel.SetActive(false);
     }
 
     void CheckEndOfLevel()
     {
         if (players[0].Count == 0 && players[1].Count == 0 && players[2].Count == 0)
         {
-            Overlay.SetActive(true);
+            OverlayNextLevel.SetActive(true);
         }
     }
 
@@ -155,7 +221,12 @@ public class GameManager : MonoBehaviour
     {
         Level++;
         pile = new List<int>();
-        LevelTextUI.GetComponent<Text>().text = "Level: " + Level;
+        LevelUI.GetComponent<Text>().text = "Level: " + Level;
 
+    }
+
+    void UpdateLivesUI()
+    {
+        LivesUI.GetComponent<Text>().text = "Lives: " + Lives;
     }
 }
