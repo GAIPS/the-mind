e04c7984cd9c67bd4030d07c6974b4cc196cedfb
Filipa Correia <filipaincorreia@gmail.com>
2021-02-05 15:39:19 +0000

    Add cross correlations to the gaze report

--- py-gaze-logs-analysis/gaze-analyser.py
+++ py-gaze-logs-analysis/gaze-analyser.py
@@ -2,6 +2,7 @@ import os
 import fnmatch
 import math
 import numpy
+from cross_correlation_by_simon_ho import cross_correlation
 
 FRAMERATE = 25
 
@@ -14,22 +15,22 @@ def GetSignalArrayOfGazingAt(gbs, target, initial_timestamp, ending_timestamp):
 	for gb in gbs:
 		if gb.target == target:
 			if gb.starting_time > last_timestamp:
-				zero_bits = int((gb.starting_time - last_timestamp) * 0.025)
+				zero_bits = int(round((gb.starting_time - last_timestamp) * 0.025))
 				for i in range(0,zero_bits):
 					array.append(0)
 
 			if gb.ending_time > ending_timestamp:
-				one_bits = int((ending_timestamp - gb.starting_time) * 0.025)
+				one_bits = int(round((ending_timestamp - gb.starting_time) * 0.025))
 				for i in range(0,one_bits):
 					array.append(1)
 				last_timestamp = ending_timestamp
 			else:
-				one_bits = int(gb.duration * 0.025)
+				one_bits = int(round(gb.duration * 0.025))
 				for i in range(0,one_bits):
 					array.append(1)
 				last_timestamp = gb.ending_time
 	if last_timestamp < ending_timestamp:
-		zero_bits = int((ending_timestamp - last_timestamp) * 0.025)
+		zero_bits = int(round((ending_timestamp - last_timestamp) * 0.025))
 		for i in range(0,zero_bits):
 			array.append(0)
 	return array
@@ -45,15 +46,15 @@ class GazeBehavior:
 
 for filename in os.listdir("./logs"):
 	reading_file = open("./logs/" + filename, "r")
-	#output_file = open("summary.txt", "w")
+	output_file = open("summary.txt", "w")
 	splited = filename.split(".")
 	name = splited[0]
 	splited = splited[1].split("at")
 	date = splited[0].split("-")
 	time = splited[1].split("-")
-	print "-----------------------------------"
-	print "| " + name + " " + date[0] + "/" + date[1] + "/" + date[2] + " - " + time[0] + ":" + time[1] + " |"
-	print "-----------------------------------"
+	output_file.write("-----------------------------------\n")
+	output_file.write("| " + name + " " + date[0] + "/" + date[1] + "/" + date[2] + " - " + time[0] + ":" + time[1] + " |\n")
+	output_file.write("-----------------------------------\n")
 
 	count_gaze = 0
 	start = False
@@ -105,7 +106,7 @@ for filename in os.listdir("./logs"):
 			count_gaze += 1
 
 	
-
+	output_file.write(">gazer" + "\t" + "target" + "\t" + "num_gazes" + "\t" + "avg_dur\n")	
 	for gazer in gazers:
 		player_gbs_per_target = []
 		for target in targets.keys():
@@ -119,11 +120,37 @@ for filename in os.listdir("./logs"):
 				avg_dur = numpy.mean(player_gbs_per_target[targets[target]])
 			else:
 				avg_dur = 0
-			print str(gazer) + "\t" + str(target) + "\t" + str(num_gazes) + "\t" + str(avg_dur)
+			output_file.write(str(gazer) + "\t" + str(target) + "\t" + str(num_gazes) + "\t" + str(avg_dur) + "\n")
 
-	var1 = GetSignalArrayOfGazingAt(players_gbs[gazers["player0"]], "mainscreen", initial_timestamp, ending_timestamp)
-	var2 = GetSignalArrayOfGazingAt(players_gbs[gazers["player1"]], "mainscreen", initial_timestamp, ending_timestamp)
-	var3 = GetSignalArrayOfGazingAt(players_gbs[gazers["player2"]], "mainscreen", initial_timestamp, ending_timestamp)
-	print len(var1)
-	print len(var2)
-	print len(var3)
+	
+
+	player0_player1 = GetSignalArrayOfGazingAt(players_gbs[gazers["player0"]], "player1", initial_timestamp, ending_timestamp)
+	player0_player2 = GetSignalArrayOfGazingAt(players_gbs[gazers["player0"]], "player2", initial_timestamp, ending_timestamp)
+	player0_mainscreen = GetSignalArrayOfGazingAt(players_gbs[gazers["player0"]], "mainscreen", initial_timestamp, ending_timestamp)
+	player1_player0 = GetSignalArrayOfGazingAt(players_gbs[gazers["player1"]], "player0", initial_timestamp, ending_timestamp)
+	player1_player2 = GetSignalArrayOfGazingAt(players_gbs[gazers["player1"]], "player2", initial_timestamp, ending_timestamp)
+	player1_mainscreen = GetSignalArrayOfGazingAt(players_gbs[gazers["player1"]], "mainscreen", initial_timestamp, ending_timestamp)
+	player2_player0 = GetSignalArrayOfGazingAt(players_gbs[gazers["player2"]], "player0", initial_timestamp, ending_timestamp)
+	player2_player1 = GetSignalArrayOfGazingAt(players_gbs[gazers["player2"]], "player1", initial_timestamp, ending_timestamp)
+	player2_mainscreen = GetSignalArrayOfGazingAt(players_gbs[gazers["player2"]], "mainscreen", initial_timestamp, ending_timestamp)
+	
+	
+	output_file.write(">GB" + "\t" + "gazers_pair" + "\t" + "max_R" + "\t" + "max_lag_adj\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_player0, player0_player2)
+	output_file.write("MUTUAL_GAZE" + "\t" + "player2_player0" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_player1, player1_player2)
+	output_file.write("MUTUAL_GAZE" + "\t" + "player2_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player0_player1, player1_player0)
+	output_file.write("MUTUAL_GAZE" + "\t" + "player0_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_mainscreen, player0_mainscreen)
+	output_file.write("JA_mainscreen" + "\t" + "player2_player0" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_mainscreen, player1_mainscreen)
+	output_file.write("JA_mainscreen" + "\t" + "player2_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player0_mainscreen, player1_mainscreen)
+	output_file.write("JA_mainscreen" + "\t" + "player0_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_player1, player0_player1)
+	output_file.write("JA_player1" + "\t" + "player2_player0" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player2_player0, player1_player0)
+	output_file.write("JA_player0" + "\t" + "player2_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")	
+	max_R, max_lag_adj, zero_R, norm_array = cross_correlation(player0_player2, player1_player2)
+	output_file.write("JA_player2" + "\t" + "player0_player1" + "\t" + str(max_R) + "\t" + str(max_lag_adj) + "\n")
